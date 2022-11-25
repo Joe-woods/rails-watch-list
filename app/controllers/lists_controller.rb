3c549e354c6a6ba8f215c7c18997fbdb1e6ac818
Joe woods <jomidl@outlook.com>
2022-11-25 16:54:31 -0500

    wtch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
+    @lists = List.all
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @lists = List.new
+  end
+
+  def create
+    @list = list.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirected_to lists_path, status: :see_other
+  end
+
+def list_params
+  params.require(:list).permit(:name, :photo)
+end
+
+
 end
